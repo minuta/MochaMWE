1ffa5c87a6bba018ff895001c6f3dacfc8b6506f
AF <gammamustang@gmail.com>
2020-10-11 22:14:39 +0200

    add test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+
+// var assert = require('assert');
+
+
+describe("pow function", function() {
+
+    it("2^3 should be 8", function() {
+      assert.equal(pow(2, 3), 8);
+    });
+});
+
+describe("pow function", function() {
+
+    it("1^2 should be 1", function() {
+      assert.equal(pow(1, 2), 1);
+    });
+});
+
