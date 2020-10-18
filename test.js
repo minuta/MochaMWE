db13fa05b0533c10f04b7df7f069dce8ef7306d4
AF <gammamustang@gmail.com>
2020-10-18 23:35:21 +0200

    reorganize tests and add a Before method

--- test.js
+++ test.js
@@ -2,17 +2,25 @@
 // var assert = require('assert');
 
 
-describe("pow function", function() {
-
-    it("2^3 should be 8", function() {
-      assert.equal(pow(2, 3), 8);
-    });
-});
 
 describe("pow function", function() {
 
-    it("1^2 should be 1", function() {
-      assert.equal(pow(1, 2), 1);
-    });
-});
+  before(function() {
+    alert("...called the Before method");
+  });
 
+  // beforeEach(() => alert("Before a test – enter a test"));
+  // afterEach(() => alert("After a test – exit a test"));
+
+  it("2^3 should be 8", function() {
+    assert.equal(pow(2, 3), 8);
+   });
+
+  it("1^2 should be 1", function() {
+    assert.equal(pow(1, 2), 1);
+  });
+
+  it("0^2 should be 1", function() {
+    assert.equal(pow(0, 2), 1);
+  });
+});
