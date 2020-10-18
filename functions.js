1924144e8d03a4449d81bb38a816c1719bced773
AF <gammamustang@gmail.com>
2020-10-18 23:34:14 +0200

    update function pow

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
 
+
 function pow(x, n) {
-    // let result = 0;
-    // for(let i=1; i<n; i++) {
-    //     result *=x;
-    // }
-    return 8;
+    if (x==0) {
+        return 1;
+    }
+    let result = x;
+    for(let i=1; i<n; i++) {
+        result *=x;
+    }
+    return result;
 }
